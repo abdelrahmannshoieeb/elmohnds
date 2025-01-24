f33a79f57e2f868d733a958657a1c8cfdbbd262d
abdelrahmannshoieeb <abdoshoieb93@gmail.com>
2025-01-24 02:38:41 +0200

    a

--- resources/views/livewire/products/elmohnds-products.blade.php
+++ resources/views/livewire/products/elmohnds-products.blade.php
@@ -186,17 +186,7 @@
                                 <div class="product-thumbnail">
                                     <div class="thumbnail">
                                         <a href="{{ route('product', $product->id) }}" class="woocommerce-LoopProduct-link woocommerce-loop-product__link">
-                                            <div class="product-main-image">
-                                                <m-image class="minimog-lazy-image" style="--lazy-image-width: 450px; --lazy-image-height: 100%; max-width: 450px; max-height: 450px;" data-image-loading>
-                                                    <m-image class="minimog-lazy-image" style="--lazy-image-width: 300px; --lazy-image-height: 450px; width: 100%; height: 100%; display: flex; justify-content: center; align-items: center;" data-image-loading>
-                                                        <img
-                                                            loading="lazy"
-                                                            src="{{ url('public/storage/' . $product->image) }}"
-                                                            style="max-width: 150px; max-height: 150px; object-fit: contain;"
-                                                            alt="365+ Food container with lid"
-                                                            class="attachment-woocommerce_thumbnail size-woocommerce_thumbnail product-main-image-img" />
-                                                    </m-image>
-                                            </div>
+                                         
 
                                             <div class="product-hover-image">
                                                 <m-image class="minimog-lazy-image" style="--lazy-image-width: 450px; --lazy-image-height: 100%; max-width: 450px; max-height: 450px;" data-image-loading>
